363219d825b77d272cc1b8cb9daaeb932af7b79b
Bruno Salmon <dev.salmonb@gmail.com>
2022-07-12 04:28:07 +0100

    Refactored webfx-platform

--- webfx-demo-medusaclock-application-gluon/src/main/java/module-info.java
+++ webfx-demo-medusaclock-application-gluon/src/main/java/module-info.java
@@ -5,9 +5,9 @@ module webfx.demo.medusaclock.application.gluon {
     // Direct dependencies modules
     requires webfx.demo.medusaclock.application;
     requires webfx.kit.openjfx;
-    requires webfx.platform.java.boot.impl;
-    requires webfx.platform.java.scheduler.impl;
-    requires webfx.platform.java.shutdown.impl;
-    requires webfx.platform.shared.log.impl.simple;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.java;
 
 }
