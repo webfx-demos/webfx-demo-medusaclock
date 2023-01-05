31cf17fb280260350989276cc729e69a59eaed3f
Bruno Salmon <dev.salmonb@gmail.com>
2023-01-05 17:59:06 +0000

    Ran webfx update with the latest version (new meta file)

--- webfx-demo-medusaclock-application-gluon/src/main/java/module-info.java
+++ webfx-demo-medusaclock-application-gluon/src/main/java/module-info.java
@@ -7,7 +7,11 @@ module webfx.demo.medusaclock.application.gluon {
     requires webfx.kit.openjfx;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
+    requires webfx.platform.resource.java;
     requires webfx.platform.scheduler.java;
-    requires webfx.platform.shutdown.java;
+    requires webfx.platform.shutdown.gluon;
+
+    // Meta Resource package
+    opens dev.webfx.platform.meta.exe;
 
 }
--- webfx-demo-medusaclock-application-openjfx/src/main/java/module-info.java
+++ webfx-demo-medusaclock-application-openjfx/src/main/java/module-info.java
@@ -7,7 +7,11 @@ module webfx.demo.medusaclock.application.openjfx {
     requires webfx.kit.openjfx;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
+    requires webfx.platform.resource.java;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.java;
+
+    // Meta Resource package
+    opens dev.webfx.platform.meta.exe;
 
 }
