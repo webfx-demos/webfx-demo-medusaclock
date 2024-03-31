f293c281816ea8522e61bb25be62c6282683ab05
Bruno Salmon <dev.salmonb@gmail.com>
2024-03-31 19:34:16 +0100

    Ran 'webfx update' (WebFX CSS + boot & service refactor)

--- webfx-demo-medusaclock-application-j2cl/src/main/resources/dev/webfx/platform/boot/j2cl/J2clEntryPoint.java
+++ webfx-demo-medusaclock-application-j2cl/src/main/resources/dev/webfx/platform/boot/j2cl/J2clEntryPoint.java
@@ -1,17 +1,18 @@
-package dev.webfx.platform.boot.j2cl.entrypoint;
+package dev.webfx.platform.boot.j2cl;
 
-import dev.webfx.platform.boot.spi.impl.j2cl.J2clApplicationBooterProvider;
+import dev.webfx.platform.boot.ApplicationBooter;
+import dev.webfx.platform.boot.spi.ApplicationBooterProvider;
 import org.treblereel.j2cl.processors.annotations.GWT3EntryPoint;
 
-import static dev.webfx.platform.javabase.emul.j2cl.ServiceRegistry.*;
+import static dev.webfx.platform.service.gwtj2cl.ServiceRegistry.*;
 
-public final class J2clEntryPoint {
+public final class J2clEntryPoint implements ApplicationBooterProvider {
 
     @GWT3EntryPoint
     public void entryPoint() {
         registerArrayConstructors();
         registerServiceProviders();
-        new J2clApplicationBooterProvider().onModuleLoad();
+        ApplicationBooter.start(this, null);
     }
 
     public static void registerArrayConstructors() {
@@ -21,12 +22,11 @@ public final class J2clEntryPoint {
     public static void registerServiceProviders() {
         register(dev.webfx.kit.launcher.spi.WebFxKitLauncherProvider.class, dev.webfx.kit.launcher.spi.impl.gwtj2cl.GwtJ2clWebFxKitLauncherProvider::new);
         register(dev.webfx.kit.mapper.spi.WebFxKitMapperProvider.class, dev.webfx.kit.mapper.spi.impl.gwtj2cl.GwtJ2clWebFxKitHtmlMapperProvider::new);
-        register(dev.webfx.platform.boot.spi.ApplicationBooterProvider.class, dev.webfx.platform.boot.spi.impl.j2cl.J2clApplicationBooterProvider::new);
         register(dev.webfx.platform.boot.spi.ApplicationModuleBooter.class, dev.webfx.kit.launcher.WebFxKitLauncherModuleBooter::new, dev.webfx.platform.boot.spi.impl.ApplicationJobsBooter::new, dev.webfx.platform.resource.spi.impl.j2cl.J2clResourceModuleBooter::new);
         register(dev.webfx.platform.console.spi.ConsoleProvider.class, dev.webfx.platform.console.spi.impl.gwtj2cl.GwtJ2clConsoleProvider::new);
         register(dev.webfx.platform.os.spi.OperatingSystemProvider.class, dev.webfx.platform.os.spi.impl.gwtj2cl.GwtJ2clOperatingSystemProvider::new);
         register(dev.webfx.platform.resource.spi.ResourceProvider.class, dev.webfx.platform.resource.spi.impl.j2cl.J2clResourceProvider::new);
-        register(dev.webfx.platform.resource.spi.impl.j2cl.J2clResourceBundle.class, dev.webfx.platform.resource.j2cl.embed.J2clEmbedResourcesBundle.ProvidedJ2clResourceBundle::new);
+        register(dev.webfx.platform.resource.spi.impl.j2cl.J2clResourceBundle.class, dev.webfx.platform.resource.j2cl.J2clEmbedResourcesBundle.ProvidedJ2clResourceBundle::new);
         register(dev.webfx.platform.scheduler.spi.SchedulerProvider.class, dev.webfx.platform.uischeduler.spi.impl.gwtj2cl.GwtJ2clUiSchedulerProvider::new);
         register(dev.webfx.platform.shutdown.spi.ShutdownProvider.class, dev.webfx.platform.shutdown.spi.impl.gwtj2cl.GwtJ2clShutdownProvider::new);
         register(dev.webfx.platform.uischeduler.spi.UiSchedulerProvider.class, dev.webfx.platform.uischeduler.spi.impl.gwtj2cl.GwtJ2clUiSchedulerProvider::new);
